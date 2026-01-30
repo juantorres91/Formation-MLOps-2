1326aa226f7cb0f44820cb2e7cb7d507f3ce3528
Juan Torres <jj.torres1443@uniandes.edu.co>
2026-01-30 15:10:25 +0000

    Interface with xgboost

--- formation_indus_ds_avancee/train_and_predict.py
+++ formation_indus_ds_avancee/train_and_predict.py
@@ -4,7 +4,7 @@ import time
 import joblib
 import mlflow
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
+import xgboost as xgb
 
 
 def train_model_with_io(features_path: str, model_registry_folder: str) -> None:
@@ -17,9 +17,17 @@ def train_model(features: pd.DataFrame, model_registry_folder: str) -> None:
     target = 'Ba_avg'
     X = features.drop(columns=[target])
     y = features[target]
+    mlflow.xgboost.autolog(log_models=True)
+
     with mlflow.start_run():
-        mlflow.sklearn.autolog(log_models=True)
-        model = RandomForestRegressor(n_estimators=1, max_depth=10, n_jobs=1)
+
+        #model = RandomForestRegressor(n_estimators=1, max_depth=10, n_jobs=1)
+        model = xgb.XGBRegressor(
+            n_estimators=100,
+            max_depth=3,
+            learning_rate=0.1,
+            random_state=42)
+
         model.fit(X, y)
     time_str = time.strftime('%Y%m%d-%H%M%S')
     joblib.dump(model, os.path.join(model_registry_folder, time_str + '.joblib'))
